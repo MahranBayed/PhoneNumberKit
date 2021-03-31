bc30d9e8911cd9f1b123ecf4e5bd54ac9d6b71c3
Mohamed Elzohirey <mohamed.elzohirey@gmail.com>
2021-03-31 14:07:54 +0200

    Update PhoneNumberTextField.swift

--- PhoneNumberKit/UI/PhoneNumberTextField.swift
+++ PhoneNumberKit/UI/PhoneNumberTextField.swift
@@ -32,15 +32,7 @@ open class PhoneNumberTextField: UITextField, UITextFieldDelegate {
             return super.text
         }
     }
-    open  var textPhone: String? {
-        set {
-            super.text = newValue
-            NotificationCenter.default.post(name: UITextField.textDidChangeNotification, object: self)
-        }
-        get {
-            return super.text
-        }
-    }
+
     /// allows text to be set without formatting
     open func setTextUnformatted(newValue: String?) {
         super.text = newValue
@@ -178,7 +170,7 @@ open class PhoneNumberTextField: UITextField, UITextFieldDelegate {
     }
 
     // MARK: Status
-    static var usedColor = UIColor(red: 0.471, green: 0.459, blue: 0.949, alpha: 1.0)
+
     public var currentRegion: String {
         return self.partialFormatter.currentRegion
     }
@@ -324,20 +316,12 @@ open class PhoneNumberTextField: UITextField, UITextFieldDelegate {
 
         self.attributedPlaceholder = ph
     }
-    public var pickerBGColor = UIColor(red: 0.471, green: 0.459, blue: 0.949, alpha: 1.0)
-    public var pickerTXTColor:UIColor = .white
-    public var navColor:UIColor = .white
-    public var navButtonsColor:UIColor = .white
-    
+    public var bgColor = UIColor(red: 0.471, green: 0.459, blue: 0.949, alpha: 1.0)
     @available(iOS 11.0, *)
     @objc func didPressFlagButton() {
         guard withDefaultPickerUI else { return }
-        PhoneNumberTextField.usedColor = pickerBGColor
         let vc = CountryCodePickerViewController(phoneNumberKit: phoneNumberKit)
-        vc.bgColor = pickerBGColor
-        vc.txtColor = pickerTXTColor
-        vc.navColor = navColor
-        vc.navButtonsColor = navButtonsColor
+        vc.bgColor = bgColor
         vc.delegate = self
         if let nav = containingViewController?.navigationController, !PhoneNumberKit.CountryCodePicker.forceModalPresentation {
             nav.pushViewController(vc, animated: true)
@@ -547,3 +531,4 @@ extension String {
 }
 
 #endif
+
